7c917b9f901136b58723e51d3a39ac8898389322
dmp-devops <seubpong.mon@truedigital.group>
2020-04-25 22:09:57 +0700

    Added nuget push local script

--- GameCoreLib/Its/Games/Core/Commons/IBoard.cs
+++ GameCoreLib/Its/Games/Core/Commons/IBoard.cs
@@ -3,6 +3,6 @@ namespace Its.Games.Core.Commons
     public interface IBoard
     {
         void Init();
-        void Move(ICoordinate coordinate, IMarker marker);
+        void Move(IMove move);
     }
 }
