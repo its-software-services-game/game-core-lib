79ebc3133b0048d432376e4d0c41d41bb0627e60
dmp-devops <seubpong.mon@truedigital.group>
2020-04-27 22:02:10 +0700

    Refactored

--- GameCoreLib/Its/Games/Core/Commons/IBoard.cs
+++ GameCoreLib/Its/Games/Core/Commons/IBoard.cs
@@ -2,7 +2,8 @@ namespace Its.Games.Core.Commons
 {
     public interface IBoard
     {
-        void Init();
-        void Move(IMove move);
+        void Init();        
+        void Put(ICoordinate coordinate, IMarker mark);
+        IMarker Get(ICoordinate coordinate);
     }
 }
--- GameCoreLib/Its/Games/Core/Commons/IRegulator.cs
+++ GameCoreLib/Its/Games/Core/Commons/IRegulator.cs
@@ -2,9 +2,11 @@ namespace Its.Games.Core.Commons
 {
     public interface IRegulator
     {
+        void Init(IBoard board);
         bool IsValidMove(IBoard board, IMove move);
         bool IsPlayable(IBoard board, IPlayer player);
         bool IsOver(IBoard board);
         IPlayer Winner { get; set; }
+        IBoard Move(IBoard board, IMove move);
     }
 }
